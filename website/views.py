38b6205183a760671be6e19305b7377950f777b5
José Cruz <124106745+josejavierc13@users.noreply.github.com>
2026-02-23 21:14:53 -0400

    Started working on different views and the authentication of users with python and flask

--- website/views.py
+++ website/views.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Welcome to Roomly!</h1><p>Your ultimate room booking solution.</p>" 
+    return render_template('index.html')
+
+@views.route('/browse')
+def browse():
+    return "<h1>Browse Listings</h1><p>Explore available student housing options.</p>"
+
+@views.route('/list-property')
+def list_property():
+    return "<h1>List Property</h1><p>Add your property details to get started.</p>"
